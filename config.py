5a553b16c13821163fc1976e94d4f8c92c4f49a7
watfahamid-prog <watfahamid@gmail.com>
2026-09-25 15:57:27 +0200

    Improve viral video automation pipeline

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DRY_RUN = os.getenv("DRY_RUN", "true").lower() == "true"
-MAX_TRENDS = int(os.getenv("MAX_TRENDS", "10"))
+MAX_TRENDS = int(os.getenv("MAX_TRENDS", "12"))
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "output")
 VIDEO_SECONDS = int(os.getenv("VIDEO_SECONDS", "15"))
@@ -10,6 +10,5 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")
 TIKTOK_RESEARCH_TOKEN = os.getenv("TIKTOK_RESEARCH_TOKEN", "")
 YOUTUBE_ENABLED = os.getenv("YOUTUBE_ENABLED", "false").lower() == "true"
 TIKTOK_ENABLED = os.getenv("TIKTOK_ENABLED", "false").lower() == "true"
-
 AI_MODE = os.getenv("AI_MODE", "template").lower()
 VIDEO_COUNT = int(os.getenv("VIDEO_COUNT", "3"))
